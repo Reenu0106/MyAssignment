90640f65307138c01485568a624d76fcb55a6472
Reenu0106 <jreenupriya@gmail.com>
2025-04-26 22:52:51 +0200

    Add files via upload
    Day1 Assignment

---
+++ Chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		
+		float version=91.0f;
+		System.out.println(version);
+		
+		String developer="Google";
+		System.out.println(developer);
+		
+		boolean isBeta=false;
+		System.out.println(isBeta);
+		
+		int release=2008;
+		System.out.println(release);
+		
+		char shortcutKey='C';
+		System.out.println(shortcutKey);
+		
+		
+
+		
+		
+		
+	}
+
+}
